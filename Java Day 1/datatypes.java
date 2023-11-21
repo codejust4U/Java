7d5d99cd83dc990ea590b9c44a8a66bdb6c0786b
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2023-11-21 12:24:53 +0545

    Add files via upload

---
+++ Java Day 1/datatypes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author panka
+ */
+public class datatypes {
+    public static void main(String args[]){
+        System.out.println("Working with Data types");
+       
+        System.out.println("\n\n Basically there are two types of data types i.e \n 1. Pritimive Data type \n Such as: boolean, char, int, byte, long, float, and double \n\n 2. Non-Primitive Data type \n Such as : String , array");
+    }
+}
