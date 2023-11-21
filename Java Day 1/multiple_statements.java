02ce8d223959d8463c52df4dfb0fcf7ad669fc4b
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2023-11-21 12:24:25 +0545

    Add files via upload

---
+++ Java Day 1/multiple_statements.java
@@ -0,0 +1,11 @@
+public class multiple_statements{
+    public static void main(String args[])
+    {
+        System.out.println("Multiple statements in Java");
+        System.out.println("1st statement");
+        System.out.println("2nd statement");
+        System.out.println("this is enough for multiple statements");
+        
+        System.out.println("Text vs Numbers: Text are included under double inverted commas where as numbe's aren't.");
+    }
+}
