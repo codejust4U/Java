7c6c93cc3ce791800aa9e52efcbe9bf44ea5fcd0
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2023-11-22 13:31:22 +0545

    Add files via upload

---
+++ Java Day 2/operators.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class operators {
+
+    public static void main(String args[]){
+        System.out.println("Operators in Java \n \n ");
+
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+    }
+}
