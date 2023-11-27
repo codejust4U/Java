55e34dfb434317ca1804f634cfb74d1bf5844b1c
Code 4 U <72220088+codejust4U@users.noreply.github.com>
2023-11-27 18:17:52 +0545

    Add files via upload

---
+++ Java Day 7/strings_.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author panka
+ */
+import java.util.Scanner;
+public class strings_ {
+    public static void main(String args[])
+    {
+        //Strings are the type of objects that can store the character of values
+        //A string acts the same as an array of characters in Java.
+        Scanner sc = new Scanner(System.in);
+        String names = sc.nextLine();
+        System.out.println("Hello "+names);
+    }
+}
